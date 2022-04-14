14b0ddd00986d9f4ca14a9c0a3ebed430fd1ec09
Lou <girardon.lou.lgm@gmail.com>
2022-04-14 16:22:30 +0200

    Order

---
+++ src/Controller/OrderController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Entity\Post;
+use App\Entity\Client;
+use App\Entity\Commande;
+use App\Entity\CommandeDetails;
+use App\Repository\ProductRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class OrderController extends AbstractController
+{
+    #[Route('/order', name: 'app_order')]
+    public function index(): Response
+    {
+        return $this->render('order/index.html.twig', [
+            'controller_name' => 'OrderController',
+        ]);
+    }
+
+    #[Route('/order/checkout', name: 'checkout_order')]
+    public function checkout(ProductRepository $productRepo, ManagerRegistry $doctrine ,SessionInterface $session): Response
+    {
+        $productsInOrder = [];
+        $totalPrice = 0;
+
+        $order = new Commande();
+        $entityManager = $doctrine->getManager();
+
+        foreach ($session->get("cart", []) as $id => $quantity) {
+            if($quantity > 0){
+                //$productsInOrder[$id] = $quantity;
+                $productsInOrder[] = $productRepo->find($id)->setQuantity($quantity);
+                $totalPrice += ($productRepo->find($id)->getPrice() * $productRepo->find($id)->getQuantity());
+                $orderLine = new CommandeDetails();
+                $orderLine->setProductQuantity($quantity);
+                $orderLine->setIdProduct($productRepo->find($id));
+                $orderLine->setIdCommande($order);
+                $entityManager->persist($orderLine);
+            }
+        }
+
+        $client = $doctrine->getRepository(Client::class)->find(1);
+
+        
+        $order->setIdClient($client);
+        $order->setCreationDate(new DateTime());
+        $order->setSendDate(new DateTime());
+        $order->setStatut("En attente de payement");
+        $order->setTotalPrice($totalPrice);
+        $entityManager->persist($order);
+
+        $entityManager->flush();
+
+        
+        dd($totalPrice);
+
+        return $this->render('order/index.html.twig', [
+            'products' =>  $productsInOrder,
+        ]);
+    }
+}
---
+++ src/Entity/Product.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $price;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'integer')]
+    private $stock;
+
+    #[ORM\OneToMany(mappedBy: 'id_product', targetEntity: CommandeDetails::class)]
+    private $commandeDetails;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $slug;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $image;
+
+    private int $quantity = 0;
+
+    public function __construct()
+    {
+        $this->commandeDetails = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+    
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+    /**
+     * @return Collection<int, CommandeDetails>
+     */
+    public function getCommandeDetails(): Collection
+    {
+        return $this->commandeDetails;
+    }
+
+    public function addCommandeDetail(CommandeDetails $commandeDetail): self
+    {
+        if (!$this->commandeDetails->contains($commandeDetail)) {
+            $this->commandeDetails[] = $commandeDetail;
+            $commandeDetail->setIdProduct($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommandeDetail(CommandeDetails $commandeDetail): self
+    {
+        if ($this->commandeDetails->removeElement($commandeDetail)) {
+            // set the owning side to null (unless already changed)
+            if ($commandeDetail->getIdProduct() === $this) {
+                $commandeDetail->setIdProduct(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
